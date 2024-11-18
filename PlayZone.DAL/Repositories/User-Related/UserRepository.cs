4f93ce1b6a1f4aa599281dd27514d531e27f4160
VoidInstant <jerome.tcherepachin@hotmail.com>
2024-11-18 16:38:01 +0100

    Debut user

---
+++ PlayZone.DAL/Repositories/User-Related/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Common;
+using PlayZone.DAL.Entities.User_Related;
+using PlayZone.DAL.Interfaces.User_Related;
+using Dapper;
+using Npgsql;
+
+namespace PlayZone.DAL.Repositories.User_Related;
+
+public class UserRepository : IUserRepository
+{
+    private readonly DbConnection _connection;
+
+    public UserRepository(DbConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public IEnumerable<User> GetAll()
+    {
+        const string Query = @"SELECT isActive, nom, prenom, email, heures_annuelles_prestables, VA, VAEX,RC FROM ""USER""";
+        return _connection.Query<User>(Query);
+    }
+
+    public User GetById(int id)
+    {
+        Const string Query = @"SELECT ID FROM ""USER""";
+    }
+
+    public User GetByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User Create(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
