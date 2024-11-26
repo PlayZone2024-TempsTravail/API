5365a1485bd03e86ee3e24ec360b8caa39e6c15f
Steven Hanse <steven.hanse@live.be>
2024-11-26 09:54:38 +0100

    Debut UserRole

---
+++ PlayZone.DAL/Entities/User_Related/UserRole.cs
@@ -0,0 +1,7 @@
+﻿namespace PlayZone.DAL.Entities.User_Related;
+
+public class UserRole
+{
+    int roleId { get; set; }
+    int userId { get; set; }
+}
---
+++ PlayZone.DAL/Repositories/User_Related/UserRoleRepository.cs
@@ -0,0 +1,38 @@
+﻿using PlayZone.DAL.Entities.User_Related;
+using PlayZone.DAL.Interfaces.User_Related;
+
+namespace PlayZone.DAL.Repositories.User_Related;
+
+public class UserRoleRepository : IUserRoleRepository
+{
+    // public IEnumerable<UserRole> GetAll()
+    // {
+    //     const string query = @"
+    //         SELECT
+    //             ""role_id"" AS ""RoleId"",
+    //             ""user_id"" AS ""UserId""
+    //         FROM ""User_Role"";
+    //     ";
+    //     return this._connection.Query<RolePermission>(query);
+    // }
+
+    public IEnumerable<UserRole> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<UserRole> GetByUser(int idRole)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Create(UserRole userRole)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(int roleId, int userId)
+    {
+        throw new NotImplementedException();
+    }
+}
