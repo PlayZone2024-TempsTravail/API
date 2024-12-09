33b54b3b060c7bd84a94e9b2dc7628faae5cbc47
Steven Hanse (Malashuy) <steven.hanse@live.be>
2024-12-09 10:44:16 +0100

    Ajout CategoryId, CategoryName, LibelleId à la sortie de PrevisionDepense

--- PlayZone.BLL/Models/Budget_Related/PrevisionBudgetLibele.cs
+++ PlayZone.BLL/Models/Budget_Related/PrevisionBudgetLibele.cs
@@ -6,7 +6,10 @@ public class PrevisionBudgetLibele
 {
     public int IdPrevisionBudgetLibele { get; set; }
     public int IdProject { get; set; }
-    public int IdLibele  { get; set; }
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public int LibeleId { get; set; }
+    public string LibeleName { get; set; }
     public DateTime Date { get; set; }
     public string Motif { get; set; }
     public decimal? Montant { get; set; }
